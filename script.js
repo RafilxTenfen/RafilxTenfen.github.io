ae5ae7531e840c3d76f96064e73424b4f62703d3
Rafael Tenfen <rafaeltenfen.rt@gmail.com>
2017-09-27 20:21:50 -0300

    f

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const IlegalArgumentError = function (msg) {
-    this.name = 'IlegalArgumentError';
-    this.message = msg;
-};
-
-function soma(a, b) {
-    if (!a || !b) throw new IlegalArgumentError('Entrada 2 Invalida');
-    return a + b;
-}
-
-try {
-    soma(2);
-} catch (err) {
-    document.querySelector('#erro').textContent = err.message;
-    alert(err.stack);
-}
+// const IlegalArgumentError = function (msg) {
+//     this.name = 'IlegalArgumentError';
+//     this.message = msg;
+// };
+//
+// function soma(a, b) {
+//     if (!a || !b) throw new IlegalArgumentError('Entrada 2 Invalida');
+//     return a + b;
+// }
+//
+// try {
+//     soma(2);
+// } catch (err) {
+//     document.querySelector('#erro').textContent = err.message;
+//     alert(err.stack);
+// }
